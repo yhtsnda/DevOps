c9b9dc5888a53cdcd114773c232f55a8a0b8b276
YoLoveLife <YoLoveLife@outlook.com>
2018-05-23 15:37:38 +0800

    删除：     apitry/__init__.py 删除：     apitry/adhoc.py 删除：     apitry/aliyun_api.py 删除：     apitry/aliyun_api_conf.py 删除：     apitry/playbook.py 删除：     apitry/sshproxyrecv.py 删除：     apitry/sshproxysend.py 删除：     apitry/walker.py apitry/

--- apitry/aliyun_api_conf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-# !/usr/bin/env python
-# Time 18-1-26
-# Author Yo
-# Email YoLoveLife@outlook.com
-
-accessKeyId = 'LTAIPgnE1RzW8pfr'
-accessSecret = 'UxhlYX9gEks35pGsDHJGtDqGw4NbGg'
